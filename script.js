55844f6a34f0014292b4c7cd3fbf55702699091f
emwolfslu <78510551+emwolfslu@users.noreply.github.com>
2021-02-15 12:46:14 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-require(["esri/Map", "esri/views/MapView"], function(Map, MapView) {
-        var map = new Map({
-          basemap: "dark-gray"
-        });
+require([
+    "esri/config",
+    "esri/Map",
+    "esri/views/MapView",
 
-        var view = new MapView({
-          container: "viewDiv",
-          map: map,
-          zoom: 10,
-          center: [-89.85, 38.85] // longitude, latitude
-        });
-      });
+    "esri/layers/FeatureLayer"
+
+  ], function(esriConfig,Map, MapView, FeatureLayer) {
+
+  esriConfig.apiKey = "AAPK242b0b70c52247b1a693f1e5cf4885c0VOLNYS8BqA2mE6WWw-9Ts4E8sFD6ZrdQEEy_pIBYouQKO6poJVlvg484pE1SbZz6";
+
+  const map = new Map({
+    basemap: "arcgis-topographic"
+  });
+
+  const view = new MapView({
+    container: "viewDiv",
+    map: map,
+    center: [-149.900284,61.218056],
+    zoom: 5
+  });
